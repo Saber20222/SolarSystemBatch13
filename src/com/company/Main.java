f6fd5048d923a2ec3cd224bb2feeaafa7a9b6eaf
Saber20222 <sabersamim.ss@gmail.com>
2022-08-27 09:28:59 -0700

    solar system ready to be deployed

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        // write code here
+
+        solarSystem inception =new solarSystem();
+        inception.sun=1;
+        inception.moon=4;
+        inception.planets=2;
+        System.out.println("the basic structure is is ready");
+    }
+}
